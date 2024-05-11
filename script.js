7aecc79c2f9e88aafeb95ee952806c21fca36d93
Zayed Alam <alamzayed2016@gmail.com>
2024-05-11 14:41:39 +0530

    008

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
 
-let currentSong = new Audio();
-let songs;
-let currFolder;
+ let currentSong = new Audio();
+ let songs;
+ let currFolder;
 
 
 
-//get song into an array 
+ //get song into an array 
 
-async function getSongs(folder) {
+ async function getSongs(folder)
+  {
     currFolder = folder;
-    console.log(folder)
+
     let a = await fetch(`/${folder}/`)
-     
+
 
     let response = await a.text();
-    console.log(response)
-    
+
+
     let div = document.createElement("div")
+
     div.innerHTML = response;
     let as = div.getElementsByTagName("a")
     songs = []
@@ -27,127 +29,136 @@ async function getSongs(folder) {
         }
     }
     //get list of all songs
-    
-    
+
+
     let songUL = document.querySelector(".songList").getElementsByTagName("ul")[0]
-    songUL.innerHTML= " "
-    
+    songUL.innerHTML = " "
+
     for (const song of songs) {
         songUL.innerHTML = songUL.innerHTML + `<li>${song.replaceAll("%20", " ")}</li>`;
-        
+
     }
-    
+
     Array.from(document.querySelector(".songList").getElementsByTagName("ul")).forEach(e => {
         // e.addEventListener("click", element => {
-            playMusic(e.firstElementChild.innerHTML.trim())
-            console.log(e.firstElementChild.innerHTML.trim())
-            // })
-        })
-        
+        playMusic(e.firstElementChild.innerHTML.trim())
+        console.log(e.firstElementChild.innerHTML.trim())
+
+
+        // })
+    })
+
+}
+
+
+// play music 
+const playMusic = (track, pause = false) => {
+    //  let audio = new audio(track)
+    currentSong.src = `/${currFolder}/` + track
+    if (!pause) {
+
+        currentSong.play()
+        play.src = "img/pause.svg"
     }
-    
-    
-    // play music 
-    const playMusic = (track, pause = false) => {
-        //  let audio = new audio(track)
-        currentSong.src = `/${currFolder}/` + track
-        if (!pause) {
-            
+    var periodIndex = track.indexOf('.');
+    var result = track.substring(0, periodIndex);
+
+    document.querySelector(".songinfo").innerHTML = decodeURI(result)
+
+
+
+
+}
+
+// load playlist according to box
+
+Array.from(document.getElementsByClassName("play")).forEach(e => {
+
+    e.addEventListener("click", async item => {
+
+console.log(`songs/${item.currentTarget.dataset.folder}`)
+        songs = await getSongs(`songs/${item.currentTarget.dataset.folder}`)
+
+
+
+    })
+
+})
+
+
+
+
+
+
+
+
+async function main() {
+
+    // await getSongs("songs/red")
+    // playMusic(songs[0], true)
+
+    // add el to  play buttons
+
+    play.addEventListener("click", () => {
+        if (currentSong.paused) {
             currentSong.play()
             play.src = "img/pause.svg"
         }
-        var periodIndex = track.indexOf('.');
-        var result = track.substring(0, periodIndex);
-        
-        document.querySelector(".songinfo").innerHTML = decodeURI(result)
-        
-        
-        
-        
-    }
+        else {
+            currentSong.pause()
+            play.src = "img/player.svg"
+        }
+    })
 
-     // load playlist according to box
-    
-     Array.from(document.getElementsByClassName("play")).forEach(e => {
-        
-        e.addEventListener("click", async item => {
+    currentSong.addEventListener("timeupdate", () => {
+        document.querySelector(".circle").style.left = (currentSong.currentTime / currentSong.duration) * 100 + "%";
+        document.querySelector(".sb").style.width = (currentSong.currentTime / currentSong.duration) * 100 + "%";
+    })
 
-           
-            songs = await getSongs(`songs/${item.currentTarget.dataset.folder}`)
-            console.log(`songs/${item.currentTarget.dataset.folder}`)
-           
-       
-        })
+
+    // seekbar el
+
+    document.querySelector(".seekbar").addEventListener("click", e => {
+        let percent = (e.offsetX / e.target.getBoundingClientRect().width) * 100;
+        document.querySelector(".circle").style.left = percent + "%";
+        document.querySelector(".sb").style.width = percent + "%";
+        currentSong.currentTime = ((currentSong.duration) * percent) / 100
+    })
+
+    // prev button
+
+    prev.addEventListener("click", () => {
+
+        songs = getSongs(currFolder)
 
     })
-    
-    async function main() {
-    
-        // await getSongs("songs/red")
-        // playMusic(songs[0], true)
-    
-        // add el to  play buttons
-    
-        play.addEventListener("click", () => {
-            if (currentSong.paused) {
-                currentSong.play()
-                play.src = "img/pause.svg"
-            }
-            else {
-                currentSong.pause()
-                play.src = "img/player.svg"
-            }
-        })
-    
-        currentSong.addEventListener("timeupdate", () => {
-            document.querySelector(".circle").style.left = (currentSong.currentTime / currentSong.duration) * 100 + "%";
-            document.querySelector(".sb").style.width = (currentSong.currentTime / currentSong.duration) * 100 + "%";
-        })
-    
-    
-        // seekbar el
-    
-        document.querySelector(".seekbar").addEventListener("click", e => {
-            let percent = (e.offsetX / e.target.getBoundingClientRect().width) * 100;
-            document.querySelector(".circle").style.left = percent + "%";
-            document.querySelector(".sb").style.width = percent + "%";
-            currentSong.currentTime = ((currentSong.duration) * percent) / 100
-        })
-    
-        // prev button
-    
-        prev.addEventListener("click", () => {
-            console.log("Prev clicked")
-            songs =  getSongs(currFolder)
-    
-        })
-    
-        // next button
-    
-        next.addEventListener("click", () => {
-            console.log("next clicked")
-            songs =  getSongs(currFolder)
-    
-    
-        })
-    
-        // volume
-    
-        document.querySelector(".range").getElementsByTagName("input")[0].addEventListener("change", (e) => {
-      
-            currentSong.volume = parseInt(e.target.value) / 100
-        })
-    
-       
-    
-    // song list 
 
-    
-    
-    
-    
-    
-    
-    }
-    main()
+    // next button
+
+    next.addEventListener("click", () => {
+
+        songs = getSongs(currFolder)
+
+
+    })
+
+    // volume
+
+    document.querySelector(".range").getElementsByTagName("input")[0].addEventListener("change", (e) => {
+
+        currentSong.volume = parseInt(e.target.value) / 100
+    })
+
+
+
+
+
+
+
+
+
+
+
+}
+main()
 
